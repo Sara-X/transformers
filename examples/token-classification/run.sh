2c7956df9810e96ea16744b33751e27bc903f330
Sara-X <43844100+Sara-X@users.noreply.github.com>
2021-03-05 13:27:52 +0800

    Update run.sh

--- examples/token-classification/run.sh
+++ examples/token-classification/run.sh
@@ -14,7 +14,8 @@
 
 python3 run_ner.py \
   --model_name_or_path bert-base-uncased \
-  --dataset_name conll2003 \
+  --train_file /content/drive/MyDrive/news recommendation/small_train.json \
+  --validation_file /content/drive/MyDrive/news recommendation/small_dev.json \
   --output_dir /tmp/test-ner \
   --do_train \
   --do_eval
