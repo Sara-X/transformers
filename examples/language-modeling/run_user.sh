7852e4125c6b482d349b06156cab15d47d2557aa
Sara-X <43844100+Sara-X@users.noreply.github.com>
2021-04-15 10:51:01 +0800

    Create run_user.sh

---
+++ examples/language-modeling/run_user.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+#
+#SBATCH --job-name=myTrainTask2GPU
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=16GB
+#SBATCH --gres=gpu:1
+#SBATCH --output=log_train_%A_%a.out
+#SBATCH --error=log_train_%A_%a.err
+#SBATCH --time=24:00:00
+source $HOME/.bashrc
+
+python3 run_clm_ner.py \
+  --model_name_or_path gpt2 \
+  --train_file /scratch/jx880/capstone/transformers/examples/language-modeling/data/train_user_entities.txt \
+  --validation_file /scratch/jx880/capstone/transformers/examples/language-modeling/data/dev_user_entities.txt \
+  --output_dir /scratch/jx880/capstone/transformers/examples/language-modeling/results \
+  --do_train \
+  --do_eval
