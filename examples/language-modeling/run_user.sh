19cdefdf99380241d4ae6282f0da3cd2a618a1ef
Sara-X <43844100+Sara-X@users.noreply.github.com>
2021-05-02 11:07:51 +0800

    Update run_user.sh

--- examples/language-modeling/run_user.sh
+++ examples/language-modeling/run_user.sh
@@ -14,8 +14,9 @@ python3 run_clm_ner.py \
   --model_name_or_path gpt2 \
   --train_file /scratch/jx880/capstone/transformers/examples/language-modeling/data/train_user_entities.txt \
   --validation_file /scratch/jx880/capstone/transformers/examples/language-modeling/data/dev_user_entities.txt \
-  --output_dir /scratch/jx880/capstone/transformers/examples/language-modeling/results \
+  --output_dir /scratch/jx880/capstone/transformers/examples/language-modeling/results_large_768 \
   --do_train \
   --do_eval \
+  --num_train_epochs 6 \
   --per_device_train_batch_size 4 \
   --per_device_eval_batch_size 4
