4d1d3aff927efae62193eb6e792dcfb4d4731602
Sara-X <43844100+Sara-X@users.noreply.github.com>
2021-05-07 12:29:21 +0800

    Create run_user_small.sh

---
+++ examples/language-modeling/run_user_small.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/bash
+#
+#SBATCH --job-name=myTrainTask2GPU
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=16GB
+#SBATCH --gres=gpu:1
+#SBATCH --output=log_train_%A_%a.out
+#SBATCH --error=log_train_%A_%a.err
+#SBATCH --time=24:00:00
+source $HOME/.bashrc
+
+python3 run_clm_ner_small.py \
+  --model_name_or_path gpt2 \
+  --train_file /scratch/jx880/capstone/transformers/examples/language-modeling/data/small/small_train_user_entities.txt \
+  --validation_file /scratch/jx880/capstone/transformers/examples/language-modeling/data/small/small_dev_user_entities.txt \
+  --output_dir /scratch/jx880/capstone/transformers/examples/language-modeling/results_small \
+  --do_train \
+  --do_eval \
+  --num_train_epochs 6 \
+  --per_device_train_batch_size 4 \
+  --per_device_eval_batch_size 4
