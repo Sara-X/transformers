9ac8d181385b1a9bf64e23545571fa8bfbbedaaa
Sara-X <43844100+Sara-X@users.noreply.github.com>
2021-05-12 16:29:58 +0800

    Create run_score.sh

---
+++ examples/language-modeling/run_score.sh
@@ -0,0 +1,14 @@
+  
+#!/usr/bin/bash
+#
+#SBATCH --job-name=myTrainTask2GPU
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=16GB
+#SBATCH --gres=gpu:1
+#SBATCH --output=log_train_%A_%a.out
+#SBATCH --error=log_train_%A_%a.err
+#SBATCH --time=24:00:00
+source $HOME/.bashrc
+
+python3 impression_score.py
